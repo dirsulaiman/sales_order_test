ce4bef751bbf05772cb2c1f7ea0b261ffb187e1c
Sulaeman <sulaiman.tsany@gmail.com>
2024-04-05 10:55:46 +0800

    add method Post RequestService.cs

--- sales_web/Services/RequestService.cs
+++ sales_web/Services/RequestService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Newtonsoft.Json;
 
 namespace SalesOrderWeb.Services
@@ -34,6 +35,31 @@ namespace SalesOrderWeb.Services
                 else
                 {
                     // Handle error
+                    throw new Exception("Error: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                throw new Exception("Error: " + ex.Message);
+            }
+        }
+        
+        public async Task Post(string path, object postData)
+        {
+            try
+            {
+                string endpoint = path;
+
+                // Serialize the post data to JSON
+                string jsonData = JsonConvert.SerializeObject(postData);
+                HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                // Send POST request and read response
+                HttpResponseMessage response = await _httpClient.PostAsync(endpoint, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
                     throw new Exception("Error: " + response.StatusCode);
                 }
             }
